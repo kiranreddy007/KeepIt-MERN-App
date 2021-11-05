68d645a432915cd05452bc25687b91adaae890ae
kiranreddy007 <r.jyothikiranreddy@gmail.com>
2021-11-05 11:38:46 +0530

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+const App=() => {
+  return (
+    <>
+
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+      <div className="App">Lets get Started</div>
+    </>
+  );
+}
+
+export default App;
