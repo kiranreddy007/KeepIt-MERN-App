0459591c79320046a616455838a3ee04ee20c7af
kiranreddy007 <r.jyothikiranreddy@gmail.com>
2021-11-13 01:08:50 +0530

    crud-frontend-setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import MyNotes from "./screens/MyNotes/MyNotes";
 import LoginScreen from "./screens/LoginScreen/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen/RegisterScreen";
+import CreateNote from "./screens/CreateNote/CreateNote";
+import SingleNote from "./screens/CreateNote/SingleNote";
 
 const App = () => {
   return (
@@ -16,7 +18,8 @@ const App = () => {
           <Route path="/" exact element={<LandingPage />} />
           <Route path="/login" exact element={<LoginScreen />} />
           <Route path="/signup" exact element={<RegisterScreen />} />
-
+          <Route path="/createnote" exact element={<CreateNote />} />
+          <Route path="/note/:id" exact element={<SingleNote />} />
           <Route path="/mynotes" element={<MyNotes />} />
         </Routes>
       </main>
--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,81 +1,101 @@
-import {Accordion, Badge, Button, Card} from 'react-bootstrap'
-import {React,useEffect, useState} from 'react'
-import { Link } from 'react-router-dom'
-import MainScreen from '../../components/MainScreen'
-
-import axios, {} from 'axios'
+import { Accordion, Badge, Button, Card } from "react-bootstrap";
+import { React, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import MainScreen from "../../components/MainScreen";
+import { useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { listNotes, deleteNoteAction } from "../../actions/notesActions";
+import Loading from "../../components/Loading";
+import ErrorMessage from "../../components/ErrorMessage";
 const MyNotes = () => {
-    const [notes,setNotes]=useState([]);
+  const dispatch = useDispatch();
 
-    const deleteHandler = (id) => {
-        if (window.confirm("Are you sure?")) {
-        //   dispatch(deleteNoteAction(id));
-        }
-      };
-    const fetchNotes = async ()=>{
-        const {data}=await axios.get("/api/notes");
-        setNotes(data);
+  const noteList = useSelector((state) => state.noteList);
+  const { loading, notes, error } = noteList;
 
-    };
-    console.log(notes);
-      useEffect(() => {
-          fetchNotes();
-      }, [])
-    return (
-        <div>
-            <MainScreen title="welcome back jk..">
-                <Link to='createnote'>
-                    <Button style={{marginLeft:10,marginBottom: 6}} size="lg">
-                        Create New Note
-                    </Button>
-                </Link>
-        
-        {
-        notes.map(note=> (
-        <Accordion>
-  <Accordion.Item eventKey={note._id}>
-    <Accordion.Header><span
-      style={{
-          color:'black',
-          textDecoration:'none',
-          flex:1,
-          curusor:'pointer',
-          alignSelf:'center',
-          fontSize:18,
-      }}
-      >{note.title}</span>
-      <div>
-      <Button href={`/note/${note._id}`}>Edit</Button>
-        <Button
-            variant="danger"
-            className="mx-2"
-            onClick={() => deleteHandler(note._id)}
-        >
-            Delete
-        </Button>
-      </div>
-      </Accordion.Header>
-    <Accordion.Body>
-    <h4>
-          <Badge variant='success' className='badge bg-success' >Category-{note.category}</Badge>
-      </h4>
-    <blockquote className="blockquote mb-0">
-      <p>
-        {note.content}
-      </p>
-      <footer className="blockquote-footer">
-        Created on-SomeDate
-      </footer>
-    </blockquote>
-    </Accordion.Body>
-  </Accordion.Item>
-  
-</Accordion>
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+  const navigate = useNavigate();
+
+  const noteCreate = useSelector((state) => state.noteCreate);
+  const { success: successCreate } = noteCreate;
+
+  const noteDelete = useSelector((state) => state.noteDelete);
+  const {
+    loading: loadingDelete,
+    error: errorDelete,
+    success: successDelete,
+  } = noteDelete;
+
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(deleteNoteAction(id));
+    }
+  };
+
+  console.log(notes);
+  useEffect(() => {
+    dispatch(listNotes());
+    if (!userInfo) {
+      navigate("/");
+    }
+  }, [dispatch, userInfo, successDelete, successCreate]);
+  return (
+    <div>
+      <MainScreen title={`welcome back ${userInfo.name}`}>
+        <Link to="/createnote">
+          <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
+            Create New Note
+          </Button>
+        </Link>
+        {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
+        {loading && <Loading />}
+        {notes?.map((note) => (
+          <Accordion>
+            <Accordion.Item eventKey={note._id}>
+              <Accordion.Header>
+                <span
+                  style={{
+                    color: "black",
+                    textDecoration: "none",
+                    flex: 1,
+                    curusor: "pointer",
+                    alignSelf: "center",
+                    fontSize: 18,
+                  }}
+                >
+                  {note.title}
+                </span>
+                <div>
+                  <Button href={`/note/${note._id}`}>Edit</Button>
+                  <Button
+                    variant="danger"
+                    className="mx-2"
+                    onClick={() => deleteHandler(note._id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </Accordion.Header>
+              <Accordion.Body>
+                <h4>
+                  <Badge variant="success" className="badge bg-success">
+                    Category-{note.category}
+                  </Badge>
+                </h4>
+                <blockquote className="blockquote mb-0">
+                  <p>{note.content}</p>
+                  <footer className="blockquote-footer">
+                    Created on {note.createdAt.substring(0, 10)}
+                  </footer>
+                </blockquote>
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
         ))}
-            </MainScreen>
-           
-        </div>
-    )
-}
+      </MainScreen>
+    </div>
+  );
+};
 
-export default MyNotes
+export default MyNotes;
